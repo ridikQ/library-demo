88832c146ea2e6a71b90aa2859fd460f4cce0326
ridikQ <ridikqendro@gmail.com>
2023-06-15 12:04:27 +0200

    Initial commit

--- src/main/java/com/example/movie1/MovieApplication.java
+++ src/main/java/com/example/movie1/MovieApplication.java
@@ -17,6 +17,8 @@ public class MovieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
+
+	dfdsf
 	@Bean
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2)
